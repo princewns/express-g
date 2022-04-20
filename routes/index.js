6ea9c93266a507359780853d488eb64fa5aa7b10
unknown <princewns@gmail.com>
2022-04-20 16:21:40 +0900

    220420

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Hello World' });
+});
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'home' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'home' });
+});
+router.post('/user', function(req, res, next) {
+  const name = req.body.name;
+  //views에 있는 동일한 이름의 ejs 파일을 찾아와서 가져온다.
+  res.render('user', { title: 'home', user: name});
+});
+
+module.exports = router;
